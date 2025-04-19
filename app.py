06a38e9744c1ae7675a5d36df3eb5f60b8218014
FotuneCat <151893967+FotuneCat@users.noreply.github.com>
2025-04-19 19:31:02 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import pipeline
+from pydantic import BaseModel, Field
+import inferless
+
+@inferless.request
+class RequestObjects(BaseModel):
+    prompt: str = Field(default="a horse near a beach")
+
+@inferless.response
+class ResponseObjects(BaseModel):
+    generated_txt: str = Field(default='Test output')
+
+class InferlessPythonModel:
+
+    def initialize(self):
+        self.generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M",device=0)
+
+    def infer(self, inputs):
+        pipeline_output = self.generator(inputs.prompt, do_sample=True, min_length=128)
+        generateObject = ResponseObjects(generated_txt = pipeline_output[0]["generated_text"])
+        return generateObject
+  
+githubFork sample repo
